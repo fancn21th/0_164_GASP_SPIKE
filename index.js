2e420de781b8efff7fc9306c2f2d50ba677710e0
范轶洁 <fanyjb@digitalchina.com>
2021-02-16 11:09:07 +0800

    Box Moving To Cursor

---
+++ index.js
@@ -0,0 +1,11 @@
+import { TweenMax } from "gsap";
+
+TweenMax.set("#box", { xPercent: -50, yPercent: -50 });
+
+document.addEventListener("click", (event) => {
+  const { clientX, clientY } = event;
+  TweenMax.to("#box", 1, {
+    x: clientX,
+    y: clientY,
+  });
+});
