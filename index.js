561ae1b9a4ffdf69a0ee63d9efd316b63e81d0dc
范轶洁 <fanyjb@digitalchina.com>
2021-02-16 14:44:57 +0800

    easeInOut

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-import { TweenMax } from "gsap";
+import { TweenMax, Elastic } from "gsap";
 
 TweenMax.to(".box", 1, {
   scale: 1.25,
   repeat: -1,
   yoyo: true,
+  ease: Elastic.easeInOut,
 });
