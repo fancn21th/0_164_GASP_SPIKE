8251f8e8bf79bae55f122a9efc466522e6e89974
范轶洁 <fanyjb@digitalchina.com>
2021-02-16 13:11:47 +0800

    kill tweens of / all

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ divs.forEach((div) => {
   document.body.appendChild(div);
 });
 
+TweenMax.to(divs, 10, { x: 100, y: 100 });
+
 document.addEventListener("click", (event) => {
-  const { x, y } = event;
-  TweenMax.to(divs, 1, { x, y });
+  TweenMax.killTweensOf(event.target);
 });
