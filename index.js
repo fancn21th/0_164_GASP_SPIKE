4704d3c528a6401b473ca80bdbcd281acf394950
范轶洁 <fanyjb@digitalchina.com>
2021-02-16 11:51:19 +0800

    from / to

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ import { TweenMax } from "gsap";
 TweenMax.set("#box", { xPercent: -50, yPercent: -50 });
 
 document.addEventListener("click", (event) => {
-  const { clientX, clientY } = event;
+  const { clientX: x, clientY: y } = event;
+  console.log(x, ":", y);
   TweenMax.to("#box", 1, {
-    x: clientX,
-    y: clientY,
+    x,
+    y,
   });
 });
+
+// document.addEventListener("click", (event) => {
+//   const { x, y } = event;
+//   console.log(x, ":", y);
+//   TweenMax.from("#box", 1, {
+//     x,
+//     y,
+//   });
+// });
